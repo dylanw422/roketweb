be11ee1b934cdba0c62d01c209d15add53b317f4
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 12:22:34 -0500

    updated middleware to handle jwt

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,31 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify, SignJWT } from "jose";
 
-export function middleware(req: NextRequest) {
-  // const { cookies } = req;
-  //
-  // const verifiedCookie = cookies.get("verified");
-  //
-  // if (!verifiedCookie && req.nextUrl.pathname.startsWith("/dashboard")) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-  //
-  // if (verifiedCookie && req.nextUrl.pathname === "/login") {
-  //   return NextResponse.redirect(new URL("/dashboard", req.url));
-  // }
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
+
+export async function middleware(req: NextRequest) {
+  const { cookies } = req;
+  const jwtCookie = cookies.get("jwt"); // Get the JWT from cookies
+
+  if (!jwtCookie) {
+    console.log("no cookie found");
+    return NextResponse.redirect(new URL("/login", req.url)); // If no JWT is found, redirect to login page
+  }
+
+  if (jwtCookie) {
+    const { payload } = await jwtVerify(jwtCookie.value, JWT_SECRET);
+    console.log(payload);
+
+    if (!payload.paid) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"],
+  matcher: ["/dashboard/:path*"], // Apply middleware to the dashboard and login pages
+  runtime: "nodejs",
 };

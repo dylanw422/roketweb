28cf848d366d465abb5f8d04668e0d1bcc1894d1
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 15:21:52 -0500

    updated to handle uuid and jwt cases

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export async function POST(req: Request) {
-  const { email, password } = await req.json();
+  const { email, password, firstName, lastName } = await req.json();
   const sql = neon(process.env.DATABASE_URL!);
 
   if (!email || !password) {
@@ -26,26 +26,36 @@ export async function POST(req: Request) {
     };
 
     if (isPasswordValid) {
-      const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+      const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "7d" });
 
       cookies().set({
         name: "jwt",
         value: token,
         httpOnly: true,
-        maxAge: 60 * 60 * 24 * 1, // 1 days
+        maxAge: 60 * 60 * 24 * 7, // 1 days
         path: "/",
       });
 
-      return Response.json(
-        { message: "Logged in.", user: user, token: token },
-        { status: 200 },
-      );
+      if (user.paid) {
+        return Response.json(
+          { message: "Logged in.", user: user },
+          { status: 200 },
+        );
+      }
+
+      if (!user.paid) {
+        return Response.json(
+          { message: "You have not purchased a license." },
+          {
+            status: 402,
+          },
+        );
+      }
     } else if (!isPasswordValid) {
-      return Response.json("Invalid email or password.", { status: 400 });
-    } else if (!user.paid) {
-      return Response.json("You have not purchased a license.", {
-        status: 400,
-      });
+      return Response.json(
+        { message: "Invalid email or password." },
+        { status: 401 },
+      );
     }
   }
 
@@ -53,32 +63,35 @@ export async function POST(req: Request) {
   // HASH PASSWORD
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  const uuid = cookies().get("uuid");
+  const uuidCookie = cookies().get("uuid");
 
   // INSERT USER INTO DATABASE
   try {
     let updatedUser;
-    if (uuid) {
+    const uuid = crypto.randomUUID();
+    if (uuidCookie) {
+      // USER PAID BEFORE MAKING ACCOUNT
       const userSearch = await sql(`SELECT * FROM users WHERE uuid = $1`, [
-        uuid.value,
+        uuidCookie.value,
       ]);
 
       const user = userSearch[0];
       if (user) {
         updatedUser = await sql(
-          `UPDATE users SET email = $1, password = $2 WHERE uuid = $3 RETURNING *`,
-          [email, hashedPassword, uuid.value],
+          `UPDATE users SET email = $1, password = $2, first_name = $3, last_name = $4 WHERE uuid = $5 RETURNING *`,
+          [email, hashedPassword, firstName, lastName, uuidCookie.value],
         );
       }
     } else {
+      // USER HAS NOT PAID
       updatedUser = await sql(
-        `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`,
-        [email, hashedPassword],
+        `INSERT INTO users (email, password, first_name, last_name, paid, uuid) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
+        [email, hashedPassword, firstName, lastName, false, uuid],
       );
 
       cookies().set({
         name: "uuid",
-        value: crypto.randomUUID(),
+        value: uuid,
         httpOnly: true,
         maxAge: 60 * 60 * 24 * 365, // 365 days
         path: "/",

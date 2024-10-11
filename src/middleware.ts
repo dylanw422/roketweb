b5a1be08b65dda3e07c11d044e9e0381782923de
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 09:27:53 -0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
 // middleware.ts
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
 
 export function middleware(req: NextRequest) {
-  const { cookies } = req;
-
-  const verifiedCookie = cookies.get("verified");
-
-  if (!verifiedCookie && req.nextUrl.pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  if (verifiedCookie && req.nextUrl.pathname === "/login") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  }
+  // const { cookies } = req;
+  //
+  // const verifiedCookie = cookies.get("verified");
+  //
+  // if (!verifiedCookie && req.nextUrl.pathname.startsWith("/dashboard")) {
+  //   return NextResponse.redirect(new URL("/login", req.url));
+  // }
+  //
+  // if (verifiedCookie && req.nextUrl.pathname === "/login") {
+  //   return NextResponse.redirect(new URL("/dashboard", req.url));
+  // }
 
   return NextResponse.next();
 }

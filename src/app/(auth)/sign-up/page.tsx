9cf1b22971fa0732b389ae6a3d095eae66711f4d
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 15:21:29 -0500

    sign up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    const res = await axios.post("/api/auth", {
+      email: email,
+      password: pw,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    if (res.status === 200) {
+      router.push("/login");
+    }
+  };
+
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">First name</Label>
+              <Input
+                onChange={(e) => setFirstName(e.target.value)}
+                id="first-name"
+                placeholder="Max"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input
+                onChange={(e) => setLastName(e.target.value)}
+                id="last-name"
+                placeholder="Robinson"
+                required
+              />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input
+              onChange={(e) => setPw(e.target.value)}
+              id="password"
+              type="password"
+            />
+          </div>
+          <Button
+            onClick={() => handleSubmit()}
+            disabled={email === "" || pw === ""}
+            type="submit"
+            className="w-full"
+          >
+            Create an account
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm"></div>
+      </CardContent>
+    </Card>
+  );
+}

d30498270d0b0ac08bb38904b7f4e51c1978f1ce
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 12:22:45 -0500

    updated auth to handle jwt

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,6 +1,9 @@
 import { neon } from "@neondatabase/serverless";
 import { cookies } from "next/headers";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
 
 export async function POST(req: Request) {
   const { email, password } = await req.json();
@@ -16,8 +19,27 @@ export async function POST(req: Request) {
     const user = users[0];
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if (isPasswordValid && user.paid) {
-      return Response.json({ message: "Logged in.", user }, { status: 200 });
+    const payload = {
+      email: user.email,
+      paid: user.paid,
+      uuid: user.uuid,
+    };
+
+    if (isPasswordValid) {
+      const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1d" });
+
+      cookies().set({
+        name: "jwt",
+        value: token,
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 1, // 1 days
+        path: "/",
+      });
+
+      return Response.json(
+        { message: "Logged in.", user: user, token: token },
+        { status: 200 },
+      );
     } else if (!isPasswordValid) {
       return Response.json("Invalid email or password.", { status: 400 });
     } else if (!user.paid) {

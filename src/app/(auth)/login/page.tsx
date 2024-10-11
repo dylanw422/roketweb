b81d1d5d010a073bc8f3619f4d62c76304b33da5
dylanw422 <dylanwestdev@gmail.com>
2024-10-11 09:25:09 -0500

    signup / signin logic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
-
-import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -11,40 +11,45 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    const res = await axios.post("/api/auth", {
+      email: email,
+      password: pw,
+    });
+
+    if (res.status === 200) {
+      router.push("/dashboard");
+    }
+  };
+
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
         <CardTitle className="text-2xl">Login</CardTitle>
         <CardDescription>
-          Enter your email below to login to your account
+          Enter your email that you used at checkout to login
         </CardDescription>
       </CardHeader>
       <CardContent>
         <div className="grid gap-4">
-          <Button variant="outline" className="w-full">
-            <Icons.google className="w-4 h-4 mr-2" />
-            Login with Google
-          </Button>
-          <Button variant="outline" className="w-full">
-            <Icons.github className="w-4 h-4 mr-2" />
-            Login with GitHub
-          </Button>
           <div className="relative">
             <div className="absolute inset-0 flex items-center">
               <span className="w-full border-t" />
-            </div>
-            <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
             </div>
           </div>
 
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
             <Input
+              onChange={(e) => setEmail(e.target.value)}
               id="email"
               type="email"
               placeholder="m@example.com"
@@ -54,20 +59,27 @@ export default function LoginForm() {
           <div className="grid gap-2">
             <div className="flex items-center">
               <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
             </div>
-            <Input id="password" type="password" required />
+            <Input
+              onChange={(e) => setPw(e.target.value)}
+              id="password"
+              type="password"
+              required
+            />
           </div>
-          <Button type="submit" className="w-full">
+          <Button
+            onClick={() => handleSubmit()}
+            disabled={email == "" || pw == ""}
+            type="submit"
+            className="w-full"
+          >
             Login
           </Button>
         </div>
         <div className="mt-4 text-center text-sm">
-          Don&apos;t have an account?{" "}
-          <Link href="/signup" className="underline">
-            Sign up
+          Forgot your password?{" "}
+          <Link href="/#pricing" className="underline">
+            Reset password
           </Link>
         </div>
       </CardContent>
